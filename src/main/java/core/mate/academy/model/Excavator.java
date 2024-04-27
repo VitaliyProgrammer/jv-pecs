95990ffde937c7a536cdaee70a91a787b95ad6ef
Vitaliy <vitaliyufc@gmail.com>
2024-04-27 22:05:58 +0300

    Done MachineList task

--- src/main/java/core/mate/academy/model/Excavator.java
+++ src/main/java/core/mate/academy/model/Excavator.java
@@ -5,7 +5,19 @@ package core.mate.academy.model;
  * Do not remove no-args constructor
  */
 public class Excavator extends Machine {
+    private String nameBrand;
+    private String model;
+    private String manufacturerCountry;
+    private int power;
+
     public Excavator() {
+    }
+
+    public Excavator(String nameBrand, String model, String manufacturerCountry, int power) {
+        this.nameBrand = nameBrand;
+        this.model = model;
+        this.manufacturerCountry = manufacturerCountry;
+        this.power = power;
     }
 
     @Override
